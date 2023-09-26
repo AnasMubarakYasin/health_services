6193ddba3e74f5d3ee8d5cf36987606d844fe25c
anasmubarakyasin <bladerlaiga.97@gmail.com>
2023-09-26 16:40:01 +0800

    chore: up version

--- config/dynamic.php
+++ config/dynamic.php
@@ -10,11 +10,11 @@ use App\Models\Midwife;
 return [
     'application' => [
         'name' => env('APP_NAME', 'Bladerlaiga'),
-        'version' => env('APP_VER', '0.5.0'),
+        'version' => env('APP_VER', '0.5.1'),
         'logo' => env('APP_LOGO', '/logo.png'),
         'favicon' => env('APP_FAV', '/favicon.ico'),
         'vendor_name' => 'Bladerlaiga',
-        'vendor_version' => '0.8.11',
+        'vendor_version' => '0.9.0',
         'vendor_year' => '2023',
         'vendor_logo' => '/logo.png',
         'template' => 'modern',
